3312e10cf0b2f809925c0790b3df282843312f09
Rob White <rob.j.white@gmail.com>
2010-11-03 19:43:01 +0000

    Initial Import

---
+++ AutoCompleteExample/AutoComplete.svc.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web;
+using System.Web.Caching;
+using System.IO;
+
+namespace AutoCompleteExample
+{
+    public class WordResult
+    {
+        public string Word { get; set; }
+        public int Length { get; set; }
+        public string Reversed { get; set; }
+    }
+
+    [ServiceContract(Namespace = "autocomplete.theoldsewingfactory.com")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class AutoComplete
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public WordResult[] WordLookup(string text, int count)
+        {
+            count = count > 20 ? 20 : count;
+            count = count < 0 ? 1 : count;
+
+            var result = (from word in GetAllWords()
+                          where word.Contains(text)
+                          select new WordResult
+                          {
+                              Word = word,
+                              Length = word.Length,
+                              Reversed = Reverse(word)
+                          })
+                          .Take(count)
+                          .ToArray();
+            return result;
+        }
+
+        public static string Reverse(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        private IList<string> GetAllWords()
+        {
+            var words = HttpContext.Current.Cache["words"] as IList<string>;
+            if (words == null)
+            {
+                var path = HttpContext.Current.Server.MapPath("~/App_Data/WordList.txt");
+
+                using (var sr = new StreamReader(path))
+                {
+                    words = new List<string>();
+                    string word;
+                    while ((word = sr.ReadLine()) != null)
+                    {
+                        words.Add(word);
+                    }
+
+                    var dependency = new CacheDependency(path);
+                    HttpContext.Current.Cache.Insert("words", words, dependency);
+                }
+            }
+
+            return words;
+        }
+    }
+}
---
+++ AutoCompleteExample/Default.aspx
@@ -0,0 +1,54 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AutoCompleteExample.Default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Simple Autocomplete Example</title>
+    <link href="Styles/jquery-ui-1.8.5.custom.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Try typing something:
+        <asp:TextBox runat="server" ID="targetTextbox" />
+        <span id="result"></span>
+    </div>
+    </form>
+    <script type="text/javascript" src="Scripts/jquery-1.4.2.min.js"></script>
+    <script type="text/javascript" src="Scripts/jquery-ui-1.8.5.custom.min.js"></script>
+    <script type="text/javascript">
+        $(function () {
+            $("input[id$='targetTextbox']").autocomplete({
+                source: function (request, response) {
+                    $.ajax({
+                        url: "AutoComplete.svc/WordLookup",
+                        data: {
+                            text: request.term,
+                            count: 15
+                        },
+                        dataType: "json",
+                        contentType: "application/json; charset=utf-8",
+                        success: function (data) {
+                            response($.map(data.d, function (item) {
+                                return {
+                                    value: item.Word,
+                                    length: item.Length,
+                                    otherWayAround: item.Reversed
+                                }
+                            }))
+                        },
+                        error: function (XMLHttpRequest, textStatus, errorThrown) {
+                            $("#result").text(textStatus);
+                        }
+                    });
+                },
+                minLength: 2,
+                select: function (event, ui) {
+                    $("#result").text("Selected: " + ui.item.value + " with " + ui.item.length + "characters, and just for fun (of a sort) backwards: " + ui.item.otherWayAround);
+                }
+            });
+        }); 
+    </script>
+</body>
+</html>
